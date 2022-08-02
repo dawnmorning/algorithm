d270aebd1caf4465860cae7eb098eb8355be5201
dawnmorning <notify9637@naver.com>
2022-08-02 22:53:33 +0900

    0802 algo

--- 1629.py
+++ 1629.py
@@ -1,13 +1,21 @@
 a,b,c = map(int,input().split())
-def cal(a, b,c):
-    if b == 1:
-        return a % c
-    value = cal(a, b//2,c)
-    if b % 2 == 1:
-        return ((value**2)*a) % c 
-    else:
-        return (value**2) % c   
-print(cal(a,b,c))
-# a,b,c = map(int,input().split())
-# n=( a ** b ) % c
-# print(n)
+# def cal(a, b,c):
+#     if b == 1:
+#         return a % c
+#     value = cal(a, b//2,c)
+#     if b % 2 == 1:
+#         return ((value**2)*a) % c 
+#     else:
+#         return (value**2) % c   
+# print(cal(a,b,c))
+def cal(a, b, c):
+    value = 1
+    while b:
+        if b == 1:
+            return a % c
+        if b % 2 == 1: value *= a
+        a = a * a
+        b //= 2
+    return value % c
+
+print(cal(10, 11, 12))
---
+++ 9086.py
@@ -0,0 +1,3 @@
+n = int(input())
+for i in range(len(n)):
+    print(i[0] + i[-1])
