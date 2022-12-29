59014379ec81b6349604d7902e9a571b5a0adc5b
김종혁 <notify9637@naver.com>
2022-12-30 08:26:30 +0900

    class3 계단오르기

---
+++ class3/계단 오르기.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+stair = [0] * 301
+for _ in range(n):
+    stair[_] = int(input())
+dp = [0] * 301
+dp[0] = stair[0]
+dp[1] = stair[0] + stair[1]
+dp[2] = max(stair[0] + stair[2], stair[1]+ stair[2])
+for i in range(3,n):
+    dp[i] = max(dp[i-3]+stair[i-1]+stair[i], dp[i-2]+stair[i])
+print(dp[n-1])
