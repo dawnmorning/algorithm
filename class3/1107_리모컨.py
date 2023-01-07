87d3e99ea6204dcf2fffc8cce567d94bfc04c265
김종혁 <notify9637@naver.com>
2023-01-07 21:36:53 +0900

    class 3

---
+++ class3/1107_리모컨.py
@@ -0,0 +1,45 @@
+# import sys
+# input = sys.stdin.readline
+# n = int(input())
+# m = int(input())
+# remote = [True for _ in range(10)]
+# if m !=0 :
+#     abnormal = list(map(int, input().split()))
+#     for i in abnormal:
+#         remote[i] = False
+# # 무지성 ++, --로 해당 값까지 가는 방법
+# cnt = abs(n-100)
+
+# for i in range(1000001):
+#     li
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+m = int(input())
+a = [True for _ in range(10)]
+
+if m != 0:
+    abnormal = list(map(int, input().split()))
+    for i in abnormal:
+        a[i] = False
+
+cnt = abs(n - 100)
+for i in range(1000001):
+    list_i = list(str(i))
+    flag = 0
+    for c in list_i:
+        if a[int(c)] == 1:
+            continue
+        else:
+            flag = 1
+            break
+
+    if flag:
+        continue
+    else:
+        # 무지성 + or - , + or - 를 눌러야할 횟수 + 누른 버튼 숫자 길이
+        cnt = min(cnt, abs(n - i) + len(list_i))
+
+print(cnt)
