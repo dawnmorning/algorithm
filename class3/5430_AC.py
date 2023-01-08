84e4caef4b410469476f94086283eb5ceaabc3bb
김종혁 <notify9637@naver.com>
2023-01-08 22:55:30 +0900

    class3

--- class3/5430_AC.py
+++ class3/5430_AC.py
@@ -1,2 +1,30 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
+tc = int(input())
+for t in range(tc):
+    flag = 0
+    func = list(input())
+    n = int(input())
+    # 미친 rstrip
+    dq = deque(input().rstrip()[1:-1].split(","))
+    if n == 0:
+        dq = deque()
+    for i in range(len(func)):
+        if func[i] == 'R':
+            flag += 1
+        elif func[i] == 'D':
+            if len(dq) >= 1:
+                if flag%2==0:
+                    dq.popleft()
+                else:
+                    dq.pop()
+            else:
+                print('error')
+                break
+    else:
+        if flag % 2 == 1:
+            dq.reverse()
+            print("["+ ",".join(dq) + "]")
+        else:
+            print("[" + ",".join(dq) + "]")
